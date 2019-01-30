88bd72046fc422969f01e774f0795f7dcc5fac2f
R K Randhir <35762823+rkrandhir@users.noreply.github.com>
2019-01-30 19:24:30 +0530

    Add files via upload

--- src/app/services/addtocart.service.ts
+++ src/app/services/addtocart.service.ts
@@ -3,39 +3,61 @@ import { Product } from "./product.model";
 import { Subject } from "rxjs";
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
 
 export class AddtocartService {
-cartItemListChanged = new Subject<Product[]>();
-private cartItemList : Product[] = [];
-private totalPrice   : number = 0;
-getTotalPrice(){
-    this.totalPrice = 0;
-    for(var i=0; i < this.cartItemList.slice().length; i++){
-        this.totalPrice += Number(this.cartItemList[i].price);
-    }
-}
+	cartItemListChanged = new Subject<Product[]>();
+	getUpdatedTotalPrice = new Subject();
+	private cartItemList: Product[] = [];
+	public totalPrice: number = 0;
 
-getCartItem(){
-    return this.cartItemList.slice();
-}
+	getTotalPrice() {
+		this.totalPrice = 0;
+		for (var i = 0; i < this.cartItemList.length; i++) {
+			this.totalPrice += this.cartItemList[i].totalItemPrice;
+		}
+		this.getUpdatedTotalPrice.next(this.totalPrice)
+	}
 
-addToCart(item: Product){
-    this.cartItemList.push(item);
-    this.getTotalPrice();
-    console.log(this.totalPrice);
-    this.cartItemListChanged.next(this.cartItemList.slice());
-}
+	getCartItem() {
+		return this.cartItemList.slice();
+	}
 
-/** Remove cart Item **/
-removeCartItem(item){
-    let index = this.cartItemList.indexOf(item);
-      if(index > -1) {
-        this.cartItemList.splice(index,1);
-      }
-      this.cartItemListChanged.next(this.cartItemList.slice());
-    } 
-    
+	/**  Add to cart from Product List **/
+	addToCart(item: Product) {
+		this.cartItemList.push(item);
+		this.cartItemListChanged.next(this.cartItemList.slice());
+		item.totalItemPrice =  item.qty * item.price; //get the total price of item
+		console.log('this.cartItemList');
+		console.log(this.cartItemList);
+		this.getTotalPrice();
+		
+	}
 
+	/** Remove cart Item **/
+	removeCartItem(item) {
+		let index = this.cartItemList.indexOf(item);
+		if (index > -1) {
+			this.cartItemList.splice(index, 1);
+		}
+		this.cartItemListChanged.next(this.cartItemList.slice());
+	}
+
+	/**  Increase cart item **/
+	increaseCartItem(item) {
+		item.qty = item.qty + 1;
+		item.totalItemPrice = item.qty * item.price;
+		console.log(item);
+	}
+	/**  Decrease cart item **/
+	decreaseCartItem(item) {
+		if (item.qty == 1) {
+			return false
+		} else {
+			item.qty = item.qty - 1;
+			item.totalItemPrice = item.qty * item.price;
+		}
+		console.log(item);
+	}
 }
--- src/app/services/product.model.ts
+++ src/app/services/product.model.ts
@@ -5,11 +5,14 @@ export class Product{
     public size: any;
     public qty: number;
     public getQty: number;
+    public totalItemPrice: number;
     public totalPrice: number;
     public imagePath: string;
+    public getUpdatedTotalPrice: number;
+
 
 constructor(
-    id: number,name: string,price: number,size: any,qty: number, getQty: number,totalPrice: number,imagePath: string
+    id: number,name: string,getUpdatedTotalPrice:number,price: number,size: any,qty: number, getQty: number,totalItemPrice: number,totalPrice: number,imagePath: string
     )
     {
         this.id = id;
@@ -18,6 +21,8 @@ constructor(
         this.size = size;
         this.qty = qty;
         this.getQty = getQty;
+        this.totalItemPrice = totalItemPrice;
+        this.getUpdatedTotalPrice = getUpdatedTotalPrice;
         this.totalPrice = totalPrice;
         this.imagePath = imagePath;
     }
